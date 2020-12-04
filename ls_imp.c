fdd029a5fef494f6b08742f1a337cad804631715
abhopla <bhoplaanubhav@gmail.com>
2020-12-03 20:45:29 -0800

    can print group names,user names and dates

--- ls_imp.c
+++ ls_imp.c
@@ -1,11 +1,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/sysmacros.h>
+ #include <time.h>
 #include <pwd.h>
 #include <grp.h>
 #include <string.h>
-#include <unistd.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define D_LEN    30 // Length of the date  
 
 void getAndPrintGroup(gid_t grpNum) {
   struct group *grp;
@@ -34,6 +41,12 @@ void getAndPrintUserName(uid_t uid) {
   }
 }
 
+void change_date(char *string, time_t *time){
+
+  strftime(string, D_LEN, "%b %e %Y %R", localtime(time));
+
+}
+
 
 int main() {
 
@@ -51,19 +64,49 @@ int main() {
   // getAndPrintUserName(10485);
 
   DIR *directory;
+  
   struct dirent *dir;
+  struct stat i_num;
+  int check = -6;
+  
+  // struct stat i_arr[100];
   
   directory = opendir(".");
   if (directory) {
-    while ((directory = readdir(directory)) != NULL) {
-      printf("%s\n", dir->d_name);
+    while ((dir = readdir(directory)) != NULL) {
+
+      printf("\n%s\n", dir->d_name);
+
+      check = stat(dir->d_name, &i_num);
+      // printf("this is the inode number: %li", i_num.st_ino);
+      getAndPrintUserName(i_num.st_uid);
+      getAndPrintGroup(i_num.st_gid);
+      //  printf("Last file modification:   %s", ctime(&i_num.st_mtime));
+
+       char *dateString = (char *)malloc(sizeof(char) * D_LEN);
+
+       change_date(dateString, &(i_num.st_mtime));
+
+       printf("%s", dateString);
+//i_num.st_ino
+    
+
+      if(check != 0){
+        printf("Error - recvfrom error: %s\n", strerror(errno));
+      }
+      // getAndPrintUserName(i_num->st_ino);
+
+    
     }
     closedir(directory);
   }else{
     printf("The directory is not valid\n");
 
   }
+  printf("\n");
 
+
+  // printf("this is the inode number: %li", i_num->st_ino);
   return(0);
 
 
